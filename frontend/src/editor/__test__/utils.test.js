42f2dbea348e14e250c7085353c960d1b21f5a47
Ilya Malyanov <imalyanov@gmail.com>
2022-05-22 13:12:15 +0545

    find the line under cursor

---
+++ frontend/src/editor/__test__/utils.test.js
@@ -0,0 +1,60 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {EditorState, ContentState, SelectionState} from "draft-js"
+
+import {getFocusKey, getFocusOffset, getTextInSelectedBlock} from "../utils"
+
+jest.mock("draft-js/lib/generateRandomKey", () => () => "test-focus-key")
+
+describe("editor utils", () => {
+  test("getFocusKey", () => {
+    const selectionState = SelectionState
+      .createEmpty()
+      .merge({
+        focusKey: "test-focus-key",
+        focusOffset: 0,
+      })
+
+    const state = EditorState
+      .createWithContent(ContentState.createFromText(""))
+
+    const stateWithSelection = EditorState.forceSelection(state, selectionState)
+
+    expect(getFocusKey(stateWithSelection)).toEqual("test-focus-key")
+  })
+
+  test("getFocusOffset", () => {
+    const selectionState = SelectionState
+      .createEmpty()
+      .merge({
+        focusKey: "test-focus-key",
+        focusOffset: 3,
+      })
+
+    const state = EditorState
+      .createWithContent(ContentState.createFromText(""))
+
+    const stateWithSelection = EditorState.forceSelection(state, selectionState)
+
+    expect(getFocusOffset(stateWithSelection)).toEqual(3)
+  })
+
+  test("getTextInSelectedBlock", () => {
+
+    const selectionState = SelectionState
+      .createEmpty()
+      .merge({
+        focusKey: "test-focus-key",
+        focusOffset: 0,
+      })
+
+    const state = EditorState
+      .createWithContent(ContentState.createFromText("test"))
+
+    const stateWithSelection = EditorState.forceSelection(state, selectionState)
+
+    expect(getTextInSelectedBlock(stateWithSelection)).toEqual("test")
+  })
+})
---
+++ frontend/src/editor/utils.js
@@ -0,0 +1,17 @@
+const getFocusKey = editorState => (
+  editorState.getSelection().getFocusKey()
+)
+
+const getFocusOffset = editorState => (
+  editorState.getSelection().getFocusOffset()
+)
+
+const getTextInSelectedBlock = editorState => (
+  editorState
+    .getCurrentContent()
+    .getBlockMap()
+    .get(getFocusKey(editorState))
+    .getText()
+)
+
+export {getFocusKey, getFocusOffset, getTextInSelectedBlock}
