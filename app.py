c4f738a72314d80da019fe7435ac2d2d38f2fd08
Ilya Malyanov <imalyanov@gmail.com>
2022-06-11 14:03:53 +0545

    add dictionary lookup

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from botok import Text
+
+import csv
+import pyewts
 
 architext = Flask(__name__, static_url_path="")
 cors = CORS(architext, resources={r"/api/*": {"origins": "http://localhost:1234"}})
+converter = pyewts.pyewts()
+
+with open("./dictionary/ranjung-yeshe.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter="|")
+    dictionary = list(reader)
 
 @architext.route("/", methods=["GET"])
 def main():
     return architext.send_static_file("index.html")
+
+@architext.route("/api/glossary", methods=["POST"])
+def glossary():
+    text = request.get_json()["text"]
+    t = Text(text)
+
+
+    words = t.tokenize_words_raw_lines.split(" ")
+    wylie = converter.toWylie(text)
+    wylie_word_list = list(filter(lambda x: x != "/", map(lambda w: w.strip(), [converter.toWylie(word) for word in words])))
+    glossary = list(map(search, wylie_word_list))
+
+    return {
+                "words": words,
+                "wylie": wylie,
+                "wylie_word_list": wylie_word_list,
+                "glossary": glossary,
+            }
+
+def search(term):
+    return list(filter(lambda td: td[0] == term, dictionary))
