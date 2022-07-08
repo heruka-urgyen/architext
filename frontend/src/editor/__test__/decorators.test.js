1f3c3243468631ee5ca695f998a6c0e78b46a6aa
Ilya Malyanov <imalyanov@gmail.com>
2022-07-08 10:52:11 +0530

    fix decorators test description

--- frontend/src/editor/__test__/decorators.test.js
+++ frontend/src/editor/__test__/decorators.test.js
@@ -7,48 +7,50 @@ import "@testing-library/jest-dom"
 
 import {createTagDecorator} from "../decorators"
 
-test("finds tags in text", () => {
-  const tags = [
-    {tag: "bo", color: "#800000"},
-    {tag: "en", color: "#0000ff"},
-  ]
+describe("decorators", () => {
+  test("finds tags in text", () => {
+    const tags = [
+      {tag: "bo", color: "#800000"},
+      {tag: "en", color: "#0000ff"},
+    ]
 
-  const decorator = createTagDecorator(tags)
-  const callback = jest.fn()
+    const decorator = createTagDecorator(tags)
+    const callback = jest.fn()
 
-  decorator._decorators.forEach(({strategy}, i) => {
-    const {tag} = tags[i]
-    strategy({getText: _ => `<${tag}>`}, callback)
-    strategy({getText: _ => `text<${tag}>`}, callback)
-    strategy({getText: _ => `<${tag}>text`}, callback)
-    strategy({getText: _ => `text<${tag}>text`}, callback)
-    strategy({getText: _ => `<${tag}></${tag}>`}, callback)
-    strategy({getText: _ => `text<${tag}></${tag}>`}, callback)
-    strategy({getText: _ => `<${tag}></${tag}>text`}, callback)
-    strategy({getText: _ => `text<${tag}></${tag}>text`}, callback)
-    strategy({getText: _ => `<${tag}>text</${tag}>`}, callback)
-    strategy({getText: _ => `<${tag}>text</${tag}>text`}, callback)
-    strategy({getText: _ => `<text${tag}>text</${tag}>`}, callback)
-    strategy({getText: _ => `<text${tag}>text</${tag}>text>`}, callback)
+    decorator._decorators.forEach(({strategy}, i) => {
+      const {tag} = tags[i]
+      strategy({getText: _ => `<${tag}>`}, callback)
+      strategy({getText: _ => `text<${tag}>`}, callback)
+      strategy({getText: _ => `<${tag}>text`}, callback)
+      strategy({getText: _ => `text<${tag}>text`}, callback)
+      strategy({getText: _ => `<${tag}></${tag}>`}, callback)
+      strategy({getText: _ => `text<${tag}></${tag}>`}, callback)
+      strategy({getText: _ => `<${tag}></${tag}>text`}, callback)
+      strategy({getText: _ => `text<${tag}></${tag}>text`}, callback)
+      strategy({getText: _ => `<${tag}>text</${tag}>`}, callback)
+      strategy({getText: _ => `<${tag}>text</${tag}>text`}, callback)
+      strategy({getText: _ => `<text${tag}>text</${tag}>`}, callback)
+      strategy({getText: _ => `<text${tag}>text</${tag}>text>`}, callback)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(tags.length * 18)
   })
 
-  expect(callback).toHaveBeenCalledTimes(tags.length * 18)
-})
+  test("creates decorator element", () => {
+    const tags = [
+      {tag: "bo", color: "#800000"},
+      {tag: "en", color: "#0000ff"},
+    ]
 
-test("creates decorator element", () => {
-  const tags = [
-    {tag: "bo", color: "#800000"},
-    {tag: "en", color: "#0000ff"},
-  ]
+    const getTag = tag => `tag tag__${tag}`
+    const decorator = createTagDecorator(tags)
 
-  const getTag = tag => `tag tag__${tag}`
-  const decorator = createTagDecorator(tags)
+    decorator._decorators.forEach(({component}, i) => {
+      const Component = component
+      const decoratorElement = render(<Component />).container.children[0]
 
-  decorator._decorators.forEach(({component}, i) => {
-    const Component = component
-    const decoratorElement = render(<Component />).container.children[0]
-
-    expect(decoratorElement).toHaveStyle({color: tags[i].color})
-    expect(decoratorElement).toHaveClass(getTag(tags[i].tag))
+      expect(decoratorElement).toHaveStyle({color: tags[i].color})
+      expect(decoratorElement).toHaveClass(getTag(tags[i].tag))
+    })
   })
 })
