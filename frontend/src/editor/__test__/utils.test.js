913e82b01c17e0c5c8089c270863fd90d4ae1824
Ilya Malyanov <imalyanov@gmail.com>
2022-08-10 11:20:58 +0700

    move getLineUnderCursor to editor utils

--- frontend/src/editor/__test__/utils.test.js
+++ frontend/src/editor/__test__/utils.test.js
@@ -8,6 +8,7 @@ import {
   getFocusKey,
   getFocusOffset,
   getTextInSelectedBlock,
+  getLineUnderCursor,
 } from "../editor-utils"
 
 jest.mock("draft-js/lib/generateRandomKey", () => () => "test-focus-key")
@@ -52,5 +53,42 @@ describe("editor utils", () => {
     const stateWithSelection = EditorState.forceSelection(state, selectionState)
 
     expect(getTextInSelectedBlock(stateWithSelection)).toEqual("test")
+  })
+
+  test("read a line wrapped in special line ending symbols", () => {
+    const chunks = [
+      "མེས་པོའི་ཤུལ་བཞག་ལས།",
+      "།མེས་པོའི་ཤུལ་བཞག་ལས།",
+      "མེས་པོའི་ཤུལ་བཞག་ལས༔",
+      "༔མེས་པོའི་ཤུལ་བཞག་ལས༔",
+    ]
+
+    const states = chunks.map(c =>
+      EditorState.createWithContent(ContentState.createFromText(c)),
+    )
+
+    expect(getLineUnderCursor(states[0])).toEqual("མེས་པོའི་ཤུལ་བཞག་ལས")
+    expect(getLineUnderCursor(states[1])).toEqual("མེས་པོའི་ཤུལ་བཞག་ལས")
+    expect(getLineUnderCursor(states[2])).toEqual("མེས་པོའི་ཤུལ་བཞག་ལས")
+    expect(getLineUnderCursor(states[3])).toEqual("མེས་པོའི་ཤུལ་བཞག་ལས")
+  })
+
+  test("get a line without special symbols based on spaces", () => {
+    const state = EditorState.createWithContent(
+      ContentState.createFromText(
+        "མེས་པོའི་ཤུལ་བཞག་ལས་ ཀློང་ཆེན་རབ་འབྱམས་ཀྱི་གསུང་འབུམ ",
+      ),
+    )
+
+    const selectionState = SelectionState.createEmpty().merge({
+      focusKey: "test-focus-key",
+      focusOffset: 40,
+    })
+
+    const stateWithSelection = EditorState.forceSelection(state, selectionState)
+
+    expect(getLineUnderCursor(stateWithSelection)).toEqual(
+      "ཀློང་ཆེན་རབ་འབྱམས་ཀྱི་གསུང་འབུམ",
+    )
   })
 })
--- frontend/src/editor/editor-utils.js
+++ frontend/src/editor/editor-utils.js
@@ -3,11 +3,50 @@ const getFocusKey = editorState => editorState.getSelection().getFocusKey()
 const getFocusOffset = editorState =>
   editorState.getSelection().getFocusOffset()
 
-const getTextInSelectedBlock = editorState =>
-  editorState
-    .getCurrentContent()
-    .getBlockMap()
-    .get(getFocusKey(editorState))
-    .getText()
+const getBlockMap = editorState => editorState.getCurrentContent().getBlockMap()
 
-export {getFocusKey, getFocusOffset, getTextInSelectedBlock}
+const getSelectedBlock = editorState =>
+  getBlockMap(editorState).get(getFocusKey(editorState))
+
+const getTextInSelectedBlock = editorState =>
+  getSelectedBlock(editorState).getText()
+
+const findLine = (text, offset) => {
+  const chunks = [...text.matchAll(/(།|༔)?(.*?)(།|༔|\s)/g)]
+  const lines = chunks.reduce(
+    (acc, chunk, i) => {
+      if (acc.res !== "") {
+        return acc
+      }
+
+      const chunkLength = [...chunk[0]].length
+      const charCountSoFar = acc.length + chunkLength
+
+      acc.length = charCountSoFar
+      acc.position = i
+
+      if (charCountSoFar <= offset) {
+        return acc
+      }
+
+      acc.res = chunk[2]
+
+      return acc
+    },
+    {position: 0, length: 0, res: ""},
+  )
+
+  return lines.res
+}
+
+const getLineUnderCursor = editorState =>
+  findLine(getTextInSelectedBlock(editorState), getFocusOffset(editorState))
+
+export {
+  getFocusKey,
+  getFocusOffset,
+  getBlockMap,
+  getSelectedBlock,
+  getTextInSelectedBlock,
+  getLineUnderCursor,
+}
