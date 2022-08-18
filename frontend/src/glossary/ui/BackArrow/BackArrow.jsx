ac2590975da05df521c31ec74650692c061ba8aa
Ilya Malyanov <imalyanov@gmail.com>
2022-08-18 09:54:11 +0700

    include jsx files in eslint

--- frontend/src/glossary/ui/BackArrow/BackArrow.jsx
+++ frontend/src/glossary/ui/BackArrow/BackArrow.jsx
@@ -1,6 +1,12 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
 function BackArrow({onMouseDown}) {
   return (
-    <h2 className="back-arrow" onMouseDown={onMouseDown}>
+    <h2
+      className="back-arrow"
+      onMouseDown={onMouseDown}
+      onKeyDown={onMouseDown}
+    >
       &#x2190;
     </h2>
   )
--- frontend/src/glossary/ui/Title/Title.jsx
+++ frontend/src/glossary/ui/Title/Title.jsx
@@ -1,18 +1,22 @@
 function Title({term, onClick}) {
   const searchable = typeof onClick === "function"
   const syllables = term.split(/\s+/)
+  const handleAction = syllable => _ => {
+    if (searchable) {
+      onClick(syllable)
+    }
+  }
 
   return (
     <h3>
       {syllables.map(syllable => (
         <span
           key={syllable}
+          role="link"
+          tabIndex={0}
           className={`term${searchable ? " searchable" : ""}`}
-          onMouseDown={_ => {
-            if (searchable) {
-              onClick(syllable)
-            }
-          }}
+          onMouseDown={handleAction(syllable)}
+          onTouch={handleAction(syllable)}
         >
           {syllable}
         </span>
