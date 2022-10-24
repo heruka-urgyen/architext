ee0583a624cc32de5cb38d3bb431d33c0ca6549f
Ilya Malyanov <imalyanov@gmail.com>
2022-10-24 14:49:52 +0530

    update backend to handle multiple dictionaries

--- app.py
+++ app.py
@@ -9,9 +9,49 @@ architext = Flask(__name__, static_url_path="")
 cors = CORS(architext, resources={r"/api/*": {"origins": "http://localhost:1234"}})
 converter = pyewts.pyewts()
 
-with open("./dictionary/ranjung-yeshe.csv", newline="") as csvfile:
-    reader = csv.reader(csvfile, delimiter="|")
-    dictionary = list(reader)
+def read_dictionary(d):
+    reader = csv.reader(d, delimiter="|")
+    return list(reader)
+
+with (
+    open("./dictionary/ranjung-yeshe.csv", newline="") as ry,
+    open("./dictionary/hopkins-sanskrit.csv", newline="") as hs,
+    open("./dictionary/dag_tshig_gsar_bsgrigs-tibetan.csv", newline="") as dt,
+    open("./dictionary/dung-dkar-tshig-mdzod-chen-mo-tibetan.csv", newline="") as dd,
+    open("./dictionary/mahavyutpatti-sanskrit.csv", newline="") as ms,
+    open("./dictionary/tshig-mdzod-chen-mo-tibetan.csv", newline="") as tm,
+    open("./dictionary/yoghacharabhumi-glossary.csv", newline="") as yg,
+):
+    dictionaries = [
+        {
+            "name": "ranjung-yeshe",
+            "dictionary": read_dictionary(ry),
+        },
+        {
+            "name": "hopkins-sanskrit",
+            "dictionary": read_dictionary(hs),
+        },
+        {
+            "name": "dag-tshig-gsar-bsgrigs-tibetan",
+            "dictionary": read_dictionary(dt),
+        },
+        {
+            "name": "dung-dkar-tshig-mdzod-chen-mo-tibetan",
+            "dictionary": read_dictionary(dd),
+        },
+        {
+            "name": "mahavyutpatti-sanskrit",
+            "dictionary": read_dictionary(ms),
+        },
+        {
+            "name": "tshig-mdzod-chen-mo-tibetan",
+            "dictionary": read_dictionary(tm),
+        },
+        {
+            "name": "yoghacharabhumi-glossary",
+            "dictionary": read_dictionary(yg),
+        },
+    ]
 
 @architext.route("/", methods=["GET"])
 def main():
@@ -22,11 +62,10 @@ def glossary():
     text = request.get_json()["text"]
     t = Text(text)
 
-
     words = t.tokenize_words_raw_lines.split(" ")
     wylie = converter.toWylie(text)
     wylie_word_list = list(filter(lambda x: x != "/", map(lambda w: w.strip(), [converter.toWylie(word) for word in words])))
-    glossary = list(map(search, set(wylie_word_list)))
+    glossary = get_defs(set(wylie_word_list))
 
     return {
                 "words": words,
@@ -35,14 +74,24 @@ def glossary():
                 "glossary": glossary,
             }
 
+
+def get_defs(terms):
+    res = []
+    for dictionary in dictionaries:
+        res.append({
+                        "dictionary": dictionary["name"],
+                        "definitions": list(map(lambda term: search(dictionary["dictionary"], term), terms))
+                   })
+    return res
+
+
 @architext.route("/api/term/<term>", methods=["GET"])
-def handleSearch(term):
+def handleSearchTerm(term):
     return {
                 "term": term,
-                "definitions": list(map(lambda r: r[1], search(term)))
+                "results": get_defs([term])
            }
 
 
-def search(term):
+def search(dictionary, term):
     return list(filter(lambda td: td[0] == term, dictionary))
-
