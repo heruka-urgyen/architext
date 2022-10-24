06fca9bc6e749004ca6c7fa371583d4b71f5df49
Ilya Malyanov <imalyanov@gmail.com>
2022-10-24 14:50:09 +0530

    update glossary frontend to handle multiple dictionaries

--- frontend/src/glossary/ui/DefinitionList/DefinitionList.jsx
+++ frontend/src/glossary/ui/DefinitionList/DefinitionList.jsx
@@ -2,23 +2,30 @@ import {DefinitionBlock} from "../DefinitionBlock"
 import {Title} from "../Title"
 
 function DefinitionList({entries, handleTitleClick}) {
-  return entries.map(terms => {
-    if (terms.length === 0) {
-      return null
-    }
-    const [[term]] = terms
-    const definitions = terms.map(([_, def]) => def)
+  return entries.map(entry =>
+    <>
+      <h1>{entry.dictionary}</h1>
 
-    const titleNode = <Title term={term} onClick={handleTitleClick} />
+      {entry.definitions.map(terms => {
+        if (terms.length === 0) {
+          return null
+        }
 
-    return (
-      <DefinitionBlock
-        key={term}
-        titleNode={titleNode}
-        definitions={definitions}
-      />
-    )
-  })
+        const [[term]] = terms
+        const definitions = terms.map(([_, def]) => def)
+        const titleNode = <Title term={term} onClick={handleTitleClick} />
+
+        return (
+          <DefinitionBlock
+            key={term}
+            titleNode={titleNode}
+            definitions={definitions}
+          />
+        )
+      })}
+    </>
+  )
+
 }
 
 export {DefinitionList}
